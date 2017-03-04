a8e40052a2a8c08085df799ba3becf037aa00444
qianqian121 <qianqian141@hotmail.com>
2017-03-04 12:15:24 -0800

    Vechicle Detection
    Vechicle Detection

--- CarND-Vehicle-Detection/process_video.py
+++ CarND-Vehicle-Detection/process_video.py
@@ -114,17 +114,17 @@ for image in images:
 # notcars = notcars[0:sample_size]
 
 ### TODO: Tweak these parameters and see how the results change.
-color_space = 'HSV' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
+color_space = 'YCrCb' # Can be RGB, HSV, LUV, HLS, YUV, YCrCb
 orient = 9  # HOG orientations
 pix_per_cell = 8 # HOG pixels per cell
 cell_per_block = 2 # HOG cells per block
-hog_channel = 0 # Can be 0, 1, 2, or "ALL"
+hog_channel = "ALL" # Can be 0, 1, 2, or "ALL"
 spatial_size = (16, 16) # Spatial binning dimensions
 hist_bins = 16    # Number of histogram bins
 spatial_feat = True # Spatial features on or off
 hist_feat = True # Histogram features on or off
 hog_feat = True # HOG features on or off
-y_start_stop = [500, 720] # Min and max in y to search in slide_window()
+y_start_stop = [400, 650] # Min and max in y to search in slide_window()
 
 car_features = extract_features(cars, color_space=color_space, 
                         spatial_size=spatial_size, hist_bins=hist_bins, 
@@ -169,40 +169,169 @@ print('Test Accuracy of SVC = ', round(svc.score(X_test, y_test), 4))
 # Check the prediction time for a single sample
 t=time.time()
 
-image = mpimg.imread('bbox-example-image.jpg')
-draw_image = np.copy(image)
+def create_windows(pyramid, image_size):
+    output = []
+    for w_size, y_lims in pyramid:
+        windows = slide_window(image_size, x_start_stop=[700, 1280], y_start_stop=y_lims,
+                        xy_window=w_size, xy_overlap=(0.75, 0.75))
+        output.extend(windows)
+    return output
 
-# Uncomment the following line if you extracted training
-# data from .png images (scaled 0 to 1 by mpimg) and the
-# image you are searching is a .jpg (scaled 0 to 255)
-#image = image.astype(np.float32)/255
+pyramid = [((64, 64),  [400, 500]),
+            ((96, 64),  [400, 500]),
+           ((96, 96),  [400, 500]),
+           ((128, 64), [400, 500]),
+           ((128, 128),[450, 578]),
+           ((192, 192),[450, 650]),
+#             ((256, 256),[450, None])
+      ]
 
-windows = slide_window(image, x_start_stop=[None, None], y_start_stop=y_start_stop, 
-                    xy_window=(96, 96), xy_overlap=(0.5, 0.5))
+# pyramid = [
+#            ((96, 96),  [400, 500])
+#            # ((128, 128),[450, 578]),
+#            # ((192, 192),[450, None]),
+# #             ((256, 256),[450, None])
+#       ]
+image_size = (720, 1280)
+windows = create_windows(pyramid, image_size)
 
-hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space, 
-                        spatial_size=spatial_size, hist_bins=hist_bins, 
-                        orient=orient, pix_per_cell=pix_per_cell, 
-                        cell_per_block=cell_per_block, 
-                        hog_channel=hog_channel, spatial_feat=spatial_feat, 
-                        hist_feat=hist_feat, hog_feat=hog_feat)                       
 
-window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)                    
+# Define a single function that can extract features using hog sub-sampling and make predictions
+def find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size, hist_bins):
+    draw_img = np.copy(img)
+    img = img.astype(np.float32) / 255
 
-plt.imshow(window_img)
-plt.show()
+    img_tosearch = img[ystart:ystop, :, :]
+    ctrans_tosearch = convert_color(img_tosearch, conv='RGB2YCrCb')
+    if scale != 1:
+        imshape = ctrans_tosearch.shape
+        ctrans_tosearch = cv2.resize(ctrans_tosearch, (np.int(imshape[1] / scale), np.int(imshape[0] / scale)))
 
-from scipy.ndimage.measurements import label
-# heat = np.zeros_like(image[:,:,0]).astype(np.float)
-heat = np.zeros_like(image[:,:,0])
-heatmap = add_heat(heat, hot_windows)
-heatmap = apply_threshold(heatmap, 1)
-labels = label(heatmap)
-print(labels[1], 'cars found')
-plt.imshow(labels[0], cmap='gray')
-plt.show()
-# Draw bounding boxes on a copy of the image
-draw_img = draw_labeled_bboxes(np.copy(image), labels)
-# Display the image
-plt.imshow(draw_img)
-plt.show()
+    ch1 = ctrans_tosearch[:, :, 0]
+    ch2 = ctrans_tosearch[:, :, 1]
+    ch3 = ctrans_tosearch[:, :, 2]
+
+    # Define blocks and steps as above
+    nxblocks = (ch1.shape[1] // pix_per_cell) - 1
+    nyblocks = (ch1.shape[0] // pix_per_cell) - 1
+    nfeat_per_block = orient * cell_per_block ** 2
+    # 64 was the orginal sampling rate, with 8 cells and 8 pix per cell
+    window = 64
+    nblocks_per_window = (window // pix_per_cell) - 1
+    cells_per_step = 2  # Instead of overlap, define how many cells to step
+    nxsteps = (nxblocks - nblocks_per_window) // cells_per_step
+    nysteps = (nyblocks - nblocks_per_window) // cells_per_step
+
+    # Compute individual channel HOG features for the entire image
+    hog1 = get_hog_features(ch1, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog2 = get_hog_features(ch2, orient, pix_per_cell, cell_per_block, feature_vec=False)
+    hog3 = get_hog_features(ch3, orient, pix_per_cell, cell_per_block, feature_vec=False)
+
+    for xb in range(nxsteps):
+        for yb in range(nysteps):
+            ypos = yb * cells_per_step
+            xpos = xb * cells_per_step
+            # Extract HOG for this patch
+            hog_feat1 = hog1[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            hog_feat2 = hog2[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            hog_feat3 = hog3[ypos:ypos + nblocks_per_window, xpos:xpos + nblocks_per_window].ravel()
+            hog_features = np.hstack((hog_feat1, hog_feat2, hog_feat3))
+
+            xleft = xpos * pix_per_cell
+            ytop = ypos * pix_per_cell
+
+            # Extract the image patch
+            subimg = cv2.resize(ctrans_tosearch[ytop:ytop + window, xleft:xleft + window], (64, 64))
+
+            # Get color features
+            spatial_features = bin_spatial(subimg, size=spatial_size)
+            hist_features = color_hist(subimg, nbins=hist_bins)
+
+            # Scale features and make a prediction
+            test_features = X_scaler.transform(
+                np.hstack((spatial_features, hist_features, hog_features)).reshape(1, -1))
+            # test_features = X_scaler.transform(np.hstack((shape_feat, hist_feat)).reshape(1, -1))
+            test_prediction = svc.predict(test_features)
+
+            if test_prediction == 1:
+                xbox_left = np.int(xleft * scale)
+                ytop_draw = np.int(ytop * scale)
+                win_draw = np.int(window * scale)
+                cv2.rectangle(draw_img, (xbox_left, ytop_draw + ystart),
+                              (xbox_left + win_draw, ytop_draw + win_draw + ystart), (0, 0, 255), 6)
+
+    return draw_img
+
+
+ystart = 400
+ystop = 656
+scale = 1.5
+
+out_img = find_cars(img, ystart, ystop, scale, svc, X_scaler, orient, pix_per_cell, cell_per_block, spatial_size,
+                    hist_bins)
+
+
+def process_image(image):
+    # global windows
+    # image = mpimg.imread('bbox-example-image.jpg')
+    draw_image = np.copy(image)
+
+    # Uncomment the following line if you extracted training
+    # data from .png images (scaled 0 to 1 by mpimg) and the
+    # image you are searching is a .jpg (scaled 0 to 255)
+    # image = image.astype(np.float32)/255
+
+    # windows = slide_window(image, x_start_stop=[700, 1280], y_start_stop=y_start_stop,
+    #                     xy_window=(96, 96), xy_overlap=(0.5, 0.5))
+
+    # window_img = draw_boxes(draw_image, windows, color=(0, 0, 255), thick=6)
+    #
+    # plt.imshow(window_img)
+    # plt.show()
+
+
+    hot_windows = search_windows(image, windows, svc, X_scaler, color_space=color_space,
+                            spatial_size=spatial_size, hist_bins=hist_bins,
+                            orient=orient, pix_per_cell=pix_per_cell,
+                            cell_per_block=cell_per_block,
+                            hog_channel=hog_channel, spatial_feat=spatial_feat,
+                            hist_feat=hist_feat, hog_feat=hog_feat)
+
+    window_img = draw_boxes(draw_image, hot_windows, color=(0, 0, 255), thick=6)
+
+    # plt.imshow(window_img)
+    # plt.show()
+
+    from scipy.ndimage.measurements import label
+    # heat = np.zeros_like(image[:,:,0]).astype(np.float)
+    heat = np.zeros_like(image[:,:,0])
+    heatmap = add_heat(heat, hot_windows)
+    heatmap = apply_threshold(heatmap, 1)
+    labels = label(heatmap)
+    print(labels[1], 'cars found')
+    plt.imshow(labels[0], cmap='gray')
+    # plt.show()
+    # Draw bounding boxes on a copy of the image
+    draw_img = draw_labeled_bboxes(np.copy(image), labels)
+    # Display the image
+    plt.imshow(draw_img)
+    # plt.show()
+    return draw_img
+
+
+# Import everything needed to edit/save/watch video clips
+from moviepy.editor import VideoFileClip
+# from IPython.display import HTML
+# myclip = VideoFileClip('project_video.mp4')
+# myclip = VideoFileClip('project_video.mp4').subclip(t_start=46.00)
+myclip = VideoFileClip('project_video.mp4').subclip(t_start=23.00)
+# myclip = VideoFileClip('challenge_video.mp4')
+# myclip = VideoFileClip('harder_challenge_video.mp4')
+# for frame in myclip.iter_frames():
+#     process_image(frame)
+
+white_output = 'white.mp4'
+# clip1 = VideoFileClip("solidWhiteRight.mp4").subclip(t_start=0)
+#clip1 = VideoFileClip("solidWhiteRight.mp4").subclip(t_start=8.32)
+white_clip = myclip.fl_image(process_image) #NOTE: this function expects color images!!
+white_clip.write_videofile(white_output, audio=False)
--- CarND-Vehicle-Detection/writeup_template.md
+++ CarND-Vehicle-Detection/writeup_template.md
@@ -1,5 +1,3 @@
-##Writeup Template
-###You can use this file as a template for your writeup if you want to submit it as a markdown file, but feel free to use some other method and submit a pdf if you prefer.
 
 ---
 
@@ -11,7 +9,7 @@ The goals / steps of this project are the following:
 * Optionally, you can also apply a color transform and append binned color features, as well as histograms of color, to your HOG feature vector. 
 * Note: for those first two steps don't forget to normalize your features and randomize a selection for training and testing.
 * Implement a sliding-window technique and use your trained classifier to search for vehicles in images.
-* Run your pipeline on a video stream and create a heat map of recurring detections frame by frame to reject outliers and follow detected vehicles.
+* Run your pipeline on a video stream (start with the test_video.mp4 and later implement on full project_video.mp4) and create a heat map of recurring detections frame by frame to reject outliers and follow detected vehicles.
 * Estimate a bounding box for vehicles detected.
 
 [//]: # (Image References)
@@ -19,8 +17,9 @@ The goals / steps of this project are the following:
 [image2]: ./examples/HOG_example.jpg
 [image3]: ./examples/sliding_windows.jpg
 [image4]: ./examples/sliding_window.jpg
-[image5]: ./examples/img50.jpg
-[image6]: ./examples/example_output.jpg
+[image5]: ./examples/bboxes_and_heat.png
+[image6]: ./examples/labels_map.png
+[image7]: ./examples/output_bboxes.png
 [video1]: ./project_video.mp4
 
 ## [Rubric](https://review.udacity.com/#!/rubrics/513/view) Points
@@ -37,7 +36,7 @@ You're reading it!
 
 ####1. Explain how (and identify where in your code) you extracted HOG features from the training images.
 
-The code for this step is contained in the first code cell of the IPython notebook (or in lines # through # of the file called `some_file.py`).  
+The code for this step is in lines # 98 through # 108 of the file called `process_video.py`.  
 
 I started by reading in all the `vehicle` and `non-vehicle` images.  Here is an example of one of each of the `vehicle` and `non-vehicle` classes:
 
@@ -66,7 +65,7 @@ I decided to search random window positions at random scales all over the image 
 
 ![alt text][image3]
 
-####2. Show some examples of test images to demonstrate how your pipeline is working.  What did you do to try to minimize false positives and reliably detect cars?
+####2. Show some examples of test images to demonstrate how your pipeline is working.  What did you do to optimize the performance of your classifier?
 
 Ultimately I searched on two scales using YCrCb 3-channel HOG features plus spatially binned color and histograms of color in the feature vector, which provided a nice result.  Here are some example images:
 
@@ -81,11 +80,21 @@ Here's a [link to my video result](./project_video.mp4)
 
 ####2. Describe how (and identify where in your code) you implemented some kind of filter for false positives and some method for combining overlapping bounding boxes.
 
-I recorded the positions of positive detections in each frame of the video.  From the positive detections I created a heatmap and then thresholded that map to identify vehicle positions.  I then used blob detection in Sci-kit Image (Determinant of a Hessian [`skimage.feature.blob_doh()`](http://scikit-image.org/docs/dev/auto_examples/plot_blob.html) worked best for me) to identify individual blobs in the heatmap and then determined the extent of each blob using [`skimage.morphology.watershed()`](http://scikit-image.org/docs/dev/auto_examples/plot_watershed.html). I then assumed each blob corresponded to a vehicle.  I constructed bounding boxes to cover the area of each blob detected.  
+I recorded the positions of positive detections in each frame of the video.  From the positive detections I created a heatmap and then thresholded that map to identify vehicle positions.  I then used `scipy.ndimage.measurements.label()` to identify individual blobs in the heatmap.  I then assumed each blob corresponded to a vehicle.  I constructed bounding boxes to cover the area of each blob detected.  
 
-Here's an example result showing the heatmap and bounding boxes overlaid on a frame of video:
+Here's an example result showing the heatmap from a series of frames of video, the result of `scipy.ndimage.measurements.label()` and the bounding boxes then overlaid on the last frame of video:
+
+### Here are six frames and their corresponding heatmaps:
 
 ![alt text][image5]
+
+### Here is the output of `scipy.ndimage.measurements.label()` on the integrated heatmap from all six frames:
+![alt text][image6]
+
+### Here the resulting bounding boxes are drawn onto the last frame in the series:
+![alt text][image7]
+
+
 
 ---
 
